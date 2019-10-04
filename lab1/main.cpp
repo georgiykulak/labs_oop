66a8067e9e18fbfd340bed143b6f04db3e1c0256
Georgiy Kulak <kulakgeorgij@gmail.com>
2019-10-04 09:52:04 +0300

    Added functions for moving elements

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -13,21 +13,30 @@ struct RingList {
         RingList(size_t size = 0);
         ~RingList();
         RingList(RingList const & obj);
-        std::string getList() const;
         T& getCurrent() const;
         T popCurrent();
         T& setCurrent(std::istream obj = std::cin);
         T& changeCurrent(T new_data);
-        void moveLeft();
-        void moveRight();
         void insertAfterCurrent();
         void insertBeforeCurrent();
+        void moveLeft();
+        void moveRight();
+ 
 };
 
 int main()
 {
         RingList<int> obj(3);
-
+        for ( auto i = 3; i; --i ) {
+                std::cout << obj.getCurrent() << "\n";
+                obj.moveRight();
+        }
+        std::cout << "\n";
+        obj.popCurrent();
+        for ( auto i = 3; i; --i ) {
+                std::cout << obj.getCurrent() << "\n";
+                obj.moveRight();
+        }
         return 0;
 }
 
@@ -43,12 +52,6 @@ RingList<T>::RingList(size_t size) :
                 curr->next = curr;
                 std::cin >> curr->data;
                 head = curr;
-                /*head = new Node;
-                head->prev = nullptr;
-                head->next = nullptr;
-                end = head;
-                curr = head;
-                curr->data = filler;*/
                 for ( auto i = size; i; --i ) {      //iteration summ == size - 1
                         temp = new Bead;
                         temp->next = head;
@@ -57,12 +60,6 @@ RingList<T>::RingList(size_t size) :
                         curr->next = temp;
                         curr = curr->next;
                         std::cin >> curr->data;
-                        /*Node* temp = new Node;
-                        temp->prev = end;
-                        temp->next = nullptr;
-                        end->next = temp;
-                        end = end->next;
-                        end->data = filler;*/
                 }
         }
 }
@@ -70,14 +67,48 @@ RingList<T>::RingList(size_t size) :
 template <class T>
 RingList<T>::~RingList()
 {
-        Bead* temp;
+        Bead* temp;               //
+        
         while ( curr->next != curr) {
                 temp = curr;
                 curr = curr->next;
                 temp = temp->prev;
                 delete temp->next;
                 temp->next = curr;
-                curr->prev = temp;
+                curr->prev = temp;//
         }
         delete curr;
+}
+
+template <class T>
+inline T& RingList<T>::getCurrent() const
+{
+        return curr->data;
+}
+
+template <class T>
+inline T RingList<T>::popCurrent()
+{
+        T tempdata = curr->data;
+        Bead* temp = curr;//
+
+        curr = curr->next;
+        temp = temp->prev;
+        delete temp->next;
+        temp->next = curr;
+        curr->prev = temp;//
+
+        return tempdata;
+}
+
+template <class T>
+inline void RingList<T>::moveLeft()
+{
+        curr = curr->prev;
+}
+
+template <class T>
+inline void RingList<T>::moveRight()
+{
+        curr = curr->next;
 }
