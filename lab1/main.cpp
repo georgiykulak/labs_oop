c1cf45bbf091ea429f4166eb8431ae10bf50834e
Georgiy Kulak <kulakgeorgij@gmail.com>
2019-10-04 09:04:46 +0300

    Added destructor

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -1,24 +1,22 @@
 #include <iostream>
 
-template <class T = long double>
+template <class T>
 struct RingList {
         private:
-        struct Node {
-                Node* prev;
-                Node* next;
+        struct Bead {
+                Bead* prev;
+                Bead* next;
                 T data;
-        } *head,
-           *end,
-          *curr;
+        } *curr;
 
         public:
-        RingList(size_t size = 0, 
-                 T filler = static_cast<T>(0));
-        RingList(RingList const & obj);
+        RingList(size_t size = 0);
         ~RingList();
-        std::string getList();
-        T& getCurrent();
+        RingList(RingList const & obj);
+        std::string getList() const;
+        T& getCurrent() const;
         T popCurrent();
+        T& setCurrent(std::istream obj = std::cin);
         T& changeCurrent(T new_data);
         void moveLeft();
         void moveRight();
@@ -28,30 +26,58 @@ struct RingList {
 
 int main()
 {
-        
+        RingList<int> obj(3);
 
         return 0;
 }
 
 template <class T>
-RingList<T>::RingList(size_t size, T filler)
+RingList<T>::RingList(size_t size) :
+        curr{nullptr}
 {
         if ( size-- ) {
-                head = new Node;
+                Bead* temp; 
+                Bead* head;
+                curr = new Bead;
+                curr->prev = curr;
+                curr->next = curr;
+                std::cin >> curr->data;
+                head = curr;
+                /*head = new Node;
                 head->prev = nullptr;
                 head->next = nullptr;
                 end = head;
                 curr = head;
-                curr->data = filler;
-                for ( auto i = size; i; --i ) {
-                        Node* temp = new Node;
+                curr->data = filler;*/
+                for ( auto i = size; i; --i ) {      //iteration summ == size - 1
+                        temp = new Bead;
+                        temp->next = head;
+                        temp->prev = curr;
+                        head->prev = temp;
+                        curr->next = temp;
+                        curr = curr->next;
+                        std::cin >> curr->data;
+                        /*Node* temp = new Node;
                         temp->prev = end;
                         temp->next = nullptr;
                         end->next = temp;
                         end = end->next;
-                        end->data = filler;
+                        end->data = filler;*/
                 }
         }
 }
 
-
+template <class T>
+RingList<T>::~RingList()
+{
+        Bead* temp;
+        while ( curr->next != curr) {
+                temp = curr;
+                curr = curr->next;
+                temp = temp->prev;
+                delete temp->next;
+                temp->next = curr;
+                curr->prev = temp;
+        }
+        delete curr;
+}
