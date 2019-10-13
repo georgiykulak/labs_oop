eb28963793616ed92f5aa76c9fc6a5648b6d8844
Georgiy Kulak <kulakgeorgij@gmail.com>
2019-10-13 20:56:57 +0300

    Add testbench

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -39,62 +39,140 @@ struct RingList {
         void _makeCurrent(T new_data);
 };
 
+inline void print(std::string s)
+{
+        std::cout << s << std::endl;
+}
+
+inline void tilda()
+{
+std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+}
+
 int main()
 {
-        /*
-        size_t numb;
-        std::cout << "First Test\n"; 
-        std::cout << "Enter number of beads for constructor:\n";
-        std::cin >> numb;
-        RingList<int> obj(numb);
-        std::cout << "Move right\n";
-        for ( auto i = numb; i; --i ) {
-                std::cout << obj.getCurrent() << "\n";
-                obj.moveRight();
+        tilda();
+        print("~~~~~~THE TESTBENCH IS STARTING...~~~~~~");
+        tilda();
+        tilda();
+        tilda();
+        print("Test 1: Default constructor");
+        tilda();
+
+        RingList<int> ring1;
+        
+        if ( ring1.empty() ) 
+                print("ring1 is empty now");
+        print("Adding three elements");
+        ring1.appendCurrent(13);
+        ring1.appendCurrent(24);
+        ring1.appendCurrent(356);
+        print("Getting elements of ring1");
+        ring1.getList();
+        tilda();
+        print("Test 2: Copy constructor");
+        tilda();
+        
+        RingList<int> ring2(ring1);
+        
+        if ( !ring2.empty() )
+                print("ring2 is not empty");
+        print("Getting elements of ring2");
+        ring2.getList();
+        tilda();
+        print("Test 3: Constructor with vector argument");
+        tilda();
+
+        std::vector<double> vctr{ 1.12, 4.25, -5.03, 23.4, 0.85 };
+        RingList<double> ring3(vctr);
+        
+        print("Getting quantity of ring3 elements");
+        std::cout << ring3.count() << "\n";
+        print("Getting elements of ring3");
+        ring3.getList();
+        tilda();
+        print("Test 4: Constructor with array argument");
+        tilda();
+
+        const size_t size = 4;
+        unsigned* arr1(new unsigned [size]);
+        for ( auto i = 0; i < size; ++i ) {
+                arr1[i] = i + 1;
         }
-        std::cout << "Move left\n";
-        for ( auto i = numb; i; --i ) {
-                std::cout << obj.getCurrent() << "\n";
-                obj.moveLeft();
+        unsigned arr2[size] { 2, 4, 6, 8 };
+
+        RingList<unsigned> r1(arr1, size);
+        RingList<unsigned> r2(arr2, size);
+        
+        delete[] arr1;
+        print("Getting ring r1 of first array");
+        print("Getting with loop and \"moveRight()\"");
+        for ( auto i = size; i; --i ) {
+                std::cout
+                << r1.getCurrent() 
+                << " ";
+                r1.moveRight();
         }
-        std::cout << "Pop\n";
-        obj.popCurrent();
-        //obj.setCurrent();
-        for ( auto i = numb - 1; i; --i ) {
-                std::cout << obj.getCurrent() << "\n";
-                obj.moveRight();
+        print("");
+        print("Analogically with function \"moveLeft()\"");
+        for ( auto i = size; i; --i ) {
+                std::cout
+                << r1.getCurrent() 
+                << " ";
+                r1.moveLeft();
         }
-        std::cout << "Set\n";
-        obj.setCurrent();
-        for ( auto i = numb; i; --i ) {
-                std::cout << obj.getCurrent() << "\n";
-                obj.moveRight();
-        }
-        std::cout << "Triple loop\n";
-        for ( auto i = numb * 3; i; --i ) {
-                std::cout << obj.getCurrent() << " ";
-                obj.moveRight();
-        }
-        std::cout << "\n";
-        */
-        //RingList<int> obj1(3);
-        //RingList<int> obj2(obj1);
-        std::vector<int> vec{1, 3, 5};
-        int arr[3] {1, 3, 5};
-        RingList<int> obj1(vec);
-        //RingList<int> obj1(3);
-        std::cout << "ok\n";
-        RingList<int> obj2(arr, 3);
-        //RingList<int> obj2(obj1);
-        std::cout << "ok\n";
-        if ( obj1.compare(obj2) ) std::cout << "true" << "\n";
-        std::cout << "ok\n";
-        std::cout << obj1.getCurrent() << " " << obj2.getCurrent() << "\n";
-        std::cout << obj1.count() << " " << obj2.count() << "\n";
-        obj1.changeCurrent(10);
-        obj1.getList();
-        obj2.getList();
-        return 0;
+        print("");
+        tilda();
+        print("Test 5: Poping and pushing elements");
+        tilda();
+        print("Poping");
+        print("Getting ring r2 of second array");
+        r2.getList();
+        print("Appending");
+        r2.appendCurrent(80);
+        print("Getting ring r2 of second array");
+        r2.getList();
+        print("Inserting before current");
+        r2.insertBeforeCurrent(60);
+        print("Getting ring r2 of second array");
+        r2.getList();
+        print("Inserting after current");
+        r2.insertAfterCurrent(70);
+        print("Getting ring r2 of second array");
+        r2.getList();
+        tilda();
+        print("Test 6: Comparing rings");
+        tilda();
+        
+        RingList<int> ring6;
+        
+        print("Appending ring6");
+        ring6.appendCurrent(13);
+        ring6.appendCurrent(24);
+        ring6.appendCurrent(356);
+        print("Moving left the ring1");
+        ring1.moveLeft();
+        print("Getting elements of ring1");
+        ring1.getList();
+        print("Getting elements of ring6");
+        ring6.getList();
+        print("Comparing ring1 with ring6");
+        if ( ring1.compare(ring6) )
+                print("They are equal");
+        else
+                print("They are NOT equal");
+        print("Comparing recursively");
+        if ( ring1.compare_all(ring6) )
+                print("They are equal");
+        else
+                print("They are NOT equal");
+        tilda();
+        tilda();
+        tilda();
+        print("~~~~~~~~~FINISH OF THE TESBENCH~~~~~~~~~");
+        tilda();
+        
+        return EXIT_SUCCESS;
 }
 
 template <class T>
@@ -191,10 +269,13 @@ inline T RingList<T>::popCurrent()
 template <class T>
 inline void RingList<T>::getList()
 {
+        moveRight();
         for ( auto i = count(); i; --i ) {
                 std::cout << getCurrent() << " ";
                 moveRight();
         }
+        moveLeft();
+        std::cout << "\n";
 }
 
 template <class T>
