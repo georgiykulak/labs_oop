f099c2a574d2fc5d2abe6a231d966b7d7924c531
Georgiy Kulak <kulakgeorgij@gmail.com>
2019-11-24 13:19:21 +0200

    Redefine return of main function

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,12 +1,11 @@
 //Сделать деление матриц
 //Сделать вместо (.. , ..) [..][..]
 //Сделать оператор присваивания
-//Сделать 5 файлов: main.cpp, source.hpp, source.cpp, testbench.hpp, testbench.cpp + Makefile
 #include "testbench.hpp"
 
 int main()
 {
         testbench();
 
-        return 0;
+        return EXIT_SUCCESS;
 }
